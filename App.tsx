b4a8cbe5bdcbeaedbb18cc2c47e1dda58406c223
frantecbh <franciscomenezesbh@gmail.com>
2022-07-18 17:59:01 -0300

    Aula 02

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+import { THEME } from './src/styles/theme';
+import Loading from './src/components/Loading';
+
+export default function App() {
+
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            {fontsLoaded ? <SignIn /> : <Loading />}
+        </NativeBaseProvider>
+
+    );
+}
+
+
